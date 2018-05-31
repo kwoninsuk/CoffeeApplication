468e056b2d90b3e3bfbaddb13b3305518537ab97
멀티1_07 <1_07@10.55.112.8>
2018-05-31 13:50:11 +0900

    한줄추가

--- src/main/java/kr/java/coffee/Main.java
+++ src/main/java/kr/java/coffee/Main.java
@@ -9,13 +9,15 @@ public class Main {
 		System.out.println("마칠시간");
 		Product p1 = new Product("A001");
 		Product p2 = new Product("A001");
-		
+
 		System.out.println(p1);
 		System.out.println(p2);
-		
-		
+
 		if (p1.equals(p2)) {
 			System.out.println("같음");
-		}else {System.out.println("다름");}
+		} else {
+			System.out.println("다름");
 		}
+		// 이 부분은 로컬에서 추가한 수정
 	}
+}
